5db905347b21f351434632a2b3a5c0ad0bf48321
siwangqishiq <525647740@qq.com>
2013-03-14 10:55:17 +0800

    push1

---
+++ NtdController/src/com/xinlan/component/Controller.java
@@ -0,0 +1,18 @@
+package com.xinlan.component;
+
+import android.graphics.Canvas;
+
+public class Controller {
+	
+	public Controller(){
+		
+	}
+	
+	public void logic(){
+		
+	}
+	
+	public void draw(Canvas canvas){
+		
+	}
+}//end class
